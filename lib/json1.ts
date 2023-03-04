3a2c67cd8dcf3586fad70f009a308da6a150cea8
Curran <curran.kelleher@gmail.com>
2023-03-04 06:30:29 -0500

    Update names

--- lib/json1.ts
+++ lib/json1.ts
@@ -36,8 +36,8 @@ function assert(pred: any, msg?: string): asserts pred {
 let debugMode = false
 
 export const type = {
-  name: 'json1',
-  uri: "http://sharejs.org/types/JSONv1",
+  name: 'p',
+  uri: 'p',
   readCursor,
   writeCursor,
 
